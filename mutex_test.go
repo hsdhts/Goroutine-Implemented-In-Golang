d7b62b270026188091d5e248e18fd6ca45fd59d6
husada20 <husadahutasoit@gmail.com>
2023-07-28 23:53:55 +0700

    goroutine

--- mutex_test.go
+++ mutex_test.go
@@ -24,3 +24,36 @@ func TestRaceConditionWithMutex(t *testing.T) {
 	time.Sleep(2 * time.Second)
 	fmt.Println("Counter", x)
 }
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.Lock()
+	balance := account.Balance
+	account.RWMutex.Unlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j:= 0; j < 100; j++ {
+				account.AddBalance(1)
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total balance", account.GetBalance())
+
+}
