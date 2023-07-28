cc9171d9adc8a0efa0fde76e7a1e4d346e341122
husada20 <husadahutasoit@gmail.com>
2023-07-28 19:02:13 +0700

    goroutine

--- channel_test.go
+++ channel_test.go
@@ -96,3 +96,33 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("SELESAI")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	for {
+
+		counter := 0
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		}
+		select {
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
