b07e9f8ce7de08943c7b2b5cf9ff4f15475ea7a0
husada20 <husadahutasoit@gmail.com>
2023-07-29 23:59:19 +0700

    goroutine

---
+++ gomaxprocs_test.go
@@ -0,0 +1,29 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGomaxProcs(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCPU := runtime.NumCPU()
+	fmt.Println("Total CPU", totalCPU)
+
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine", totalGoroutine)
+}
