37e720ad7934c76e4df1da7ac82bf551c40bf65b
husada20 <husadahutasoit@gmail.com>
2023-07-29 22:06:13 +0700

    goroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -2,6 +2,7 @@ package golang_goroutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -28,4 +29,38 @@ func TestManyGoroutine(t *testing.T) {
 	}
 
 	time.Sleep(5 * time.Second)
+}
+
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Lock", user1.Name)
+	user1.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	fmt.Println("Lock", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
 }
